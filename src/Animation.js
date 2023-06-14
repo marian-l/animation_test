be8490a0a6be3b39d6eb54e2354eedc6dfef85af
marianlippold <marian.lippold@students.hs-mainz.de>
2023-06-14 14:10:24 +0200

    penis

---
+++ src/Animation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import './tstyles.css'
+
+export default function Animation() {
+    let [view, setView] = useState(false);
+    function show() {
+        setView(true)
+    }
+    function hide() {
+        setView(false)
+    }
+
+    return (
+        <div>
+            <div className="modal" style={{top: view && '5px'}}>
+                <p className="modal-header">THIS is a modal header</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. </p>
+                <div className="btn-box">
+                    <button onClick={hide}></button>
+                </div>
+            </div>
+            <div className="btn-box">
+                <button onClick={show}></button>
+            </div>
+        </div> 
+    )
+}
---
+++ src/Transition.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import './style.css';
+
+export default function Transition() {
+    let [mode, setMode] = useState("false");
+    function handleClick() {
+        setMode(mode == 'light' ? 'dark' : 'light')
+    }
+
+    return (
+        <div className={'app ' + mode}>
+            <h1>This is a Day/Night Toggle Website</h1>
+            <div className="btn-box">
+                <button onClick={handleClick}>Toggle</button>
+            </div>
+        </div>
+    );
+}
